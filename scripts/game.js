fb8c9380e6081d906906e9a57324c7a8132d2b11
rafaelvottoni <rafaelottoni@gmail.com>
2021-07-09 18:12:45 -0300

    add time and moves conuter, modify style

--- scripts/game.js
+++ scripts/game.js
@@ -1,50 +1,45 @@
 let game = {
-  lockMode: false, //se uma carta estiver aberta ela não pode ser clicada novamente e se tiver duas abertas nenhuma poderá ser aberta, então nesses momentos o lockMode será true
-  firstCard: null, //a primeira carta aberta começa como vazio
-  secondCard: null, //a segunda carta aberta começa como vazio também
+  lockMode: false,
+  firstCard: null,
+  secondCard: null,
 
-  techs: [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
+  chars: [
+    'c3po',
+    'chewbacca',
+    'darthvader',
+    'hansolo',
+    'leia',
+    'luke',
+    'obi',
+    'r2d2',
+    'stormtrooper',
+    'yoda'
   ],
 
   setCard: function (id) {
-    //setCard retorna verdadeiro ou falso
-    let card = this.cards.filter(card => card.id === id)[0] //saber se a carta ta virada ou não. e essa função retornara todas cartas que tem o id igual ao id que eu cliquei. o filter retorna um array, então pegarei so o elemento de index 0 dele
+    let card = this.cards.filter(card => card.id === id)[0]
 
     if (card.flipped || this.lockMode) {
-      //se essa carta está virada retorna false:
       return false
     }
 
     if (!this.firstCard) {
-      //se fistcard for nulo ele receberá o card clicado
       this.firstCard = card
       this.firstCard.flipped = true
       return true
     } else {
-      //se a primeira cartão nao for null ele vai tentar colocar na segunda carta:
       this.secondCard = card
       this.secondCard.flipped = true
-      this.lockMode = true //se colocar na segunda carta o jogo entra em lockMode
+      this.lockMode = true
       return true
     }
   },
 
   checkMatch: function () {
     if (!this.firstCard || !this.secondCard) {
-      //se não tiver primeira ou segunda carta ele retorna false
       return false
     }
-    return this.firstCard.icon === this.secondCard.icon //vai retornar verdadeiro ou falso
+    return this.firstCard.icon === this.secondCard.icon
   },
 
   clearCards: function () {
@@ -54,75 +49,59 @@ let game = {
   },
 
   unflipCards: function () {
-    //tirar o flip das cartas
     this.firstCard.flipped = false
     this.secondCard.flipped = false
     this.clearCards()
   },
 
   checkGameOver: function () {
-    return this.cards.filter(card => !card.flipped).length === 0 //esse filter vai me retornar um array com todas as cartas que estão com flipped false, se o tamanho do array for 0 essa comparação vai ser true. e o checkGameOver será true.
+    return this.cards.filter(card => !card.flipped).length === 0
   },
 
-  cards: null, //não tem nada nas cartas antes de executar a função startGame
+  cards: null,
 
-  //Criando cards para cada uma das techs do array techs:
+  createCardsFromChars: function () {
+    this.cards = []
 
-  createCardsFromTechs: function () {
-    this.cards = [] //cards será um array com outros 10 arrays dentro (pq são 10 techs), e cada um desses 10 arrays terão 2 objetos (os pares)
-
-    this.techs.forEach(tech => {
-      //pegando cada tech das minhas techs e criando um par
-      this.cards.push(this.createPairFromTech(tech)) //vai adicionar ao array cards, o que essa função me entregar.
+    this.chars.forEach(char => {
+      this.cards.push(this.createPairFromChar(char))
     })
 
-    this.cards = this.cards.flatMap(pair => pair) //Eu nao quero cards como um array com 10 arrays, então a funçaõ flatMap desmembra esses arrays de dentro, fazendo cards ser um array com 20 objetos.
+    this.cards = this.cards.flatMap(pair => pair)
     this.shuffleCards()
     return this.cards
   },
 
-  createPairFromTech: function (tech) {
-    //ela vai retornar um array com dois objetos, o card e seu par
+  createPairFromChar: function (char) {
     return [
       {
-        //o id será criado aleatoriamente e concatenado com o nome da tech, usando essa funçao:
-        id: this.createIdWithTech(tech),
-        icon: tech,
-        //flipped me diz se a carta estara virada ou não
+        id: this.createIdWithChar(char),
+        icon: char,
+
         flipped: false
       },
       {
-        id: this.createIdWithTech(tech),
-        icon: tech,
+        id: this.createIdWithChar(char),
+        icon: char,
         flipped: false
       }
     ]
   },
 
-  createIdWithTech: function (tech) {
-    //a função vai retornar a tech em questão concatenada com o numeor aleatorio
-    return tech + parseInt(Math.random() * 1000)
+  createIdWithChar: function (char) {
+    return char + parseInt(Math.random() * 1000)
   },
   shuffleCards: function (cards) {
-    //função para embaralhar as cartas de um array
-    let currentIndex = this.cards.length //index da ultima carta do array (no caso cards)
-    let randonIndex = 0 //index aleatorio do array
+    let currentIndex = this.cards.length
+    let randonIndex = 0
 
     while (currentIndex !== 0) {
-      //enquanto currentIndex for diferente de 0 eu vou fazer isso:
-      // o motivo de ser diferente de 0 é pq o lenght do array começa no 1 e o index começa no 0, então o ultimo item do array tera index 19 e o lenght do array sera 20.
-
-      randonIndex = Math.floor(Math.random() * currentIndex) //eu so posso pegar cartas que não foram embaralhadas, então ele pega o math.random que da um valor de 0 até 1 e multiplicando pelo current index que foi a ultima carta a ser embaralhada, e o floor garante que o numero escolhido sera um anterior. se der 4.99 ele pega o 4.
-
+      randonIndex = Math.floor(Math.random() * currentIndex)
       currentIndex--
-
-      //Isso aqui vai pegar o cards com o index que foi criado aleatoriamnte e o current index da vez e troca-los de lugar dentro do array:
       ;[this.cards[randonIndex], this.cards[currentIndex]] = [
         this.cards[currentIndex],
         this.cards[randonIndex]
       ]
     }
-
-    //pela a questão da referencia no javascript, nessa função não precisa retornar nada, pq ao passar o array cards para a função ela ja modifica o proprio array cards, ela não cria um novo array. tem um parte sobre referencias no material de javascript avançado. ele fala sobre modificar copias de array e com isso modificar o array original também
   }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,106 @@ const BACK = 'card-back'
 const CARD = 'card'
 const ICON = 'icon'
 
+let seconds = 0
+let minutes = 0
+let timeInSeconds = 0
+let interval = null
+
+let moves = 0
+let formatMoves
+
+const Timer = {
+  updateBestTime: function () {
+    let bestTimeHTML = document.querySelector('.best')
+    let getBestTimeStorage = localStorage.getItem('time')
+    let formatBestTime
+
+    let formatMinutes = Math.trunc(parseInt(getBestTimeStorage) / 60)
+
+    let formatSeconds = Math.trunc(parseInt(getBestTimeStorage) % 60)
+
+    if (formatMinutes < 10 && formatSeconds < 10) {
+      formatBestTime = `0${formatMinutes}:0${formatSeconds}`
+    } else if (formatMinutes < 10 && formatSeconds >= 10) {
+      formatBestTime = `0${formatMinutes}:${formatSeconds}`
+    } else if (formatMinutes >= 10 && formatSeconds < 10) {
+      formatBestTime = `${formatMinutes}:0${formatSeconds}`
+    } else {
+      formatBestTime = `${formatMinutes}:${formatSeconds}`
+    }
+
+    let bestTimeStorage = getBestTimeStorage != null ? formatBestTime : '00:00'
+    bestTimeHTML.innerHTML = bestTimeStorage
+  },
+  calculateTime: function () {
+    if (minutes < 10) {
+      document.getElementById('minutes').innerHTML = '0' + minutes
+    }
+
+    seconds++
+    if (seconds == 60) {
+      minutes++
+
+      seconds = 0
+
+      if (minutes < 10) {
+        document.getElementById('minutes').innerHTML = '0' + minutes
+      } else {
+        document.getElementById('minutes').innerHTML = minutes
+      }
+    }
+
+    if (seconds < 10) {
+      document.getElementById('seconds').innerHTML = '0' + seconds
+    } else {
+      document.getElementById('seconds').innerHTML = seconds
+    }
+  },
+  startTime: function () {
+    interval = setInterval(this.calculateTime, 1000)
+  },
+  stopTime: function () {
+    clearInterval(interval)
+
+    timeInSeconds = minutes * 60 + seconds
+
+    let getBestTimeStorage = localStorage.getItem('time')
+
+    if (getBestTimeStorage == null || timeInSeconds < getBestTimeStorage) {
+      localStorage.setItem('time', timeInSeconds)
+    }
+
+    setTimeout(this.updateBestTime, 50)
+
+    minutes = 0
+
+    seconds = 0
+  }
+}
+
 startGame()
 
 function startGame() {
-  initializeCards(game.createCardsFromTechs()) //criando model visual da carta para cada uma das cards
+  Timer.updateBestTime()
+  updateBestMoves()
+  Timer.startTime()
+  initializeCards(game.createCardsFromChars())
 }
 
 function initializeCards(cards) {
-  let gameBoard = document.getElementById('gameBoard') //pegando o tabuleiro no html
+  let gameBoard = document.getElementById('gameBoard')
 
-  gameBoard.innerHTML = '' //limpando o gameboard antes de adicionar as cartas
+  gameBoard.innerHTML = ''
 
   game.cards.forEach(card => {
-    //para todos os elementos sera criado um elemento no html, no caso é uma div, essa div vai receber um id que é o proprio id do card e a class card:
     let cardElement = document.createElement('div')
     cardElement.id = card.id
-    cardElement.classList.add(CARD) //CARD variavel inicializada com uma string 'card'
-    cardElement.dataset.icon = card.icon //vai adicionar um dataset icon no card (ex: data-icon="bootstrap" ) e esse dataset será igual ao icon de cada card
+    cardElement.classList.add(CARD)
+    cardElement.dataset.icon = card.icon
     createCardContent(card, cardElement)
 
-    cardElement.addEventListener('click', flipCard) //adicionando o evento de clique a todas as cartas que executará a função flipCard
-    gameBoard.appendChild(cardElement) //adicionando cada elemento dentro do tabuleiro, colocando "filhos" dentro do pai gameBoard
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement)
   })
 }
 
@@ -33,18 +112,15 @@ function createCardContent(card, cardElement) {
 }
 
 function createCardFace(face, card, element) {
-  //criando as divs de frente e verso das cartas
-  let cardElementFace = document.createElement('div') //vai criar a div dentro da card
-  cardElementFace.classList.add(face) //adicionar a classe
+  let cardElementFace = document.createElement('div')
+  cardElementFace.classList.add(face)
 
   if (face == FRONT) {
-    //se a face for front adiciona a imagem
     let iconElement = document.createElement('img')
-    iconElement.classList.add(ICON) //variavel criada no inicio do codigo também
-    iconElement.src = './assets/images/' + card.icon + '.png' //concatenação, para o endereço da imagem, o icon tem o mesmo nome da imagem e é uma das propriedades do objeto card (id, icon e flipped)
+    iconElement.classList.add(ICON)
+    iconElement.src = './assets/images/' + card.icon + '.png'
     cardElementFace.appendChild(iconElement)
   } else {
-    //senão adiciona esse texto
     cardElementFace.innerHTML = '&lt/&gt'
   }
 
@@ -52,30 +128,33 @@ function createCardFace(face, card, element) {
 }
 
 function flipCard() {
+  let movesInHTML = document.querySelector('.number-of-moves')
+
   if (game.setCard(this.id)) {
-    //se conseguir colocar a carta ele executa o classList.add
     this.classList.add('flip')
-    //flipa a carta que eu clicar
+
+    moves++
+    formatMoves = Math.trunc(moves / 2)
+    movesInHTML.innerHTML = formatMoves
 
     if (game.secondCard) {
-      //apenas se eu tiver a secondCard que farei isso:
       if (game.checkMatch()) {
-        //se checkMatch for verdadeiro executa o clearcards
         game.clearCards()
         if (game.checkGameOver()) {
+          Timer.stopTime()
+          setBestMovesInLocalStorage()
           let gameOverLayer = document.getElementById('gameOver')
-          gameOverLayer.style.display = 'flex' //mudar o display do gameOver para flex. ele está none
+          gameOverLayer.style.display = 'flex'
         }
       } else {
-        //acrescentando um intervalo para executar essa função
         setTimeout(() => {
           let firstCardView = document.getElementById(game.firstCard.id)
           let secondCardView = document.getElementById(game.secondCard.id)
 
-          firstCardView.classList.remove('flip') //remover a class flip
+          firstCardView.classList.remove('flip')
           secondCardView.classList.remove('flip')
-          game.unflipCards() //executa o flipcards que também executa um clearcards
-          game.clearCards() //executa o clear cards
+          game.unflipCards()
+          game.clearCards()
         }, 800)
       }
     }
@@ -86,5 +165,24 @@ function restart() {
   game.clearCards()
   startGame()
   let gameOverLayer = document.getElementById('gameOver')
-  gameOverLayer.style.display = 'none' //mudar o display do gameOver para none. ele está flex
+  gameOverLayer.style.display = 'none'
+}
+
+function updateBestMoves() {
+  let bestMoveHTML = document.querySelector('.best-moves')
+  let getBestTimeStorage = localStorage.getItem('moves')
+
+  let bestMovesStorage = getBestTimeStorage != null ? getBestTimeStorage : 0
+
+  bestMoveHTML.innerHTML = bestMovesStorage
+}
+
+function setBestMovesInLocalStorage() {
+  let getBestMovesStorage = localStorage.getItem('moves')
+
+  if (getBestMovesStorage == null || formatMoves < getBestMovesStorage) {
+    localStorage.setItem('moves', formatMoves)
+  }
+
+  moves = 0
 }
